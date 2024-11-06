440af2ba0236747a9080edbc19b83fc4b28fbb64
Bencheikh Ilias <ilias.bencheikh@etu.u-paris.fr>
2024-11-06 12:16:02 +0100

    Deadline 5/11

--- lib/Regle.ml
+++ lib/Regle.ml
@@ -1,4 +1,4 @@
-let navire_plateau_1 = ("Torpilleur",1,5)::[]
+let navire_plateau_1 = ("PorteAvion",1,5)::("Croiseur",1,4)::("ContreTorpilleur",2,3)::("SousMarin",1,3)::("Torpilleur",1,2)::[]
 let navire_plateau_2 = ("PorteAvion",1,5)::("Croiseur",1,4)::("ContreTorpilleur",2,3)::("SousMarin",1,3)::("Torpilleur",1,2)::[]
 
 
